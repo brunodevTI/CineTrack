a1c6e5cf99e2e649ef5f0280d13453a98195bbed
Bruno <brunoms2083@gmail.com>
2025-08-06 17:40:08 -0300

    initial commit

---
+++ public/assets/scripts/script.js
@@ -0,0 +1,15 @@
+
+dados = []
+
+document.addEventListener("DOMContentLoaded", function() {
+
+
+    detalheFilmes();
+    document.querySelector('#imgcarousel').classList.add('imgfade');
+    mudarIndex();
+    
+
+});
+
+
+
